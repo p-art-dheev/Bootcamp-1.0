766417db13868ff15b05b7ca6c39caa7da290291
p-art-dheev <pardheev.vatturu1234@gmail.com>
2026-01-06 17:05:47 +0530

    JS Date Methods

--- Web3/00-Warmup/05-Date.js
+++ Web3/00-Warmup/05-Date.js
@@ -1,9 +1,9 @@
 let d = new Date(); // get the date
-console.log(d)
-console.log(d.getDate())
-console.log(d.getMonth()+1) // month range 0-11, so add +1 to it
-console.log(d.getFullYear()) // year
-console.log(d.getTime()) // milli seconds since jan 1970
-console.log(d.getSeconds()) // seconds
-console.log(d.getMinutes()) // Minutes
-console.log(d.getHours()) // Hours 0-24 hr
+console.log("Date is " + d);
+console.log("Date is " + d.getDate());
+console.log("Month is " + d.getMonth() + 1); // month range 0-11, so add +1 to it
+console.log("Year is " + d.getFullYear()); // year
+console.log("Milli seconds since 1970 jan is " + d.getTime()); // milli seconds since jan 1970
+console.log("Seconds is " + d.getSeconds()); // seconds
+console.log("Minutes is " + d.getMinutes()); // Minutes
+console.log("Hours is " + d.getHours()); // Hours 0-24 hr
