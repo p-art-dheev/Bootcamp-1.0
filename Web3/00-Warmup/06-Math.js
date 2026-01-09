e0dedf8b78aa42ac004a5d7818363e465c179606
p-art-dheev <pardheev.vatturu1234@gmail.com>
2026-01-09 21:57:04 +0530

    Math JS Updated

--- Web3/00-Warmup/06-Math.js
+++ Web3/00-Warmup/06-Math.js
@@ -2,5 +2,28 @@
 let a = Math.random(); // Random number between 0 and < 1
 console.log(a);
 
-// Rounds Down
-console.log(Math.floor(4.9));
+console.log(Math.floor(Math.random()*10)+1) // Random number between 1-10
+
+// Math.floor - Rounds Down
+console.log(Math.floor(4.9)); //4
+console.log(Math.floor(-1.9)); //-2
+
+// Math.ceil - Rounds Up
+console.log(Math.ceil(11.2)); //12
+console.log(Math.ceil(-5.6)); //-5
+
+// Math.max - Maximum of all the values
+console.log(Math.max(1, 2, 100)); //100
+console.log(Math.max(-11, -2, -1)); //-1
+
+// Math.min - Minimum of all the values
+console.log(Math.min(1, 2, 3)); //1
+console.log(Math.min(-99, 0.11, 2, 3)); //-99
+
+
+// Math.sqrt - Square Root of the numbers 
+console.log(Math.sqrt(400)) //20
+
+// Math.pov - Power of the number wrt to the base
+console.log(Math.pow(10,2)) //100
+console.log(Math.pow(7.50,2)) //56.25
